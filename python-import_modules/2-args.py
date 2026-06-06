38eab182a133617fbba1692a9b56fd1d2eb3d888
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-06-06 19:53:41 +0000

    Task 2: Print the number of and the list of arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    num_args = len(sys.argv) - 1
+
+    if num_args == 0:
+        print("{} arguments.".format(num_args))
+    elif num_args == 1:
+        print("{} argument:".format(num_args))
+    else:
+        print("{} arguments:".format(num_args))
+
+    for i in range(1, num_args + 1):
+        print("{}: {}".format(i, sys.argv[i]))
