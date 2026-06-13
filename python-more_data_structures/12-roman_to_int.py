303486afefb81e4bee22f13b5f0e112b7bae6f08
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-06-13 19:30:37 +0000

    Fix pycodestyle trailing whitespace and line length

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,19 +2,22 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-        
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
     total = 0
     prev_value = 0
-    
+
     for char in reversed(roman_string):
         current_value = roman_dict.get(char, 0)
-        
+
         if current_value < prev_value:
             total -= current_value
         else:
             total += current_value
-            
+
         prev_value = current_value
-        
+
     return total
