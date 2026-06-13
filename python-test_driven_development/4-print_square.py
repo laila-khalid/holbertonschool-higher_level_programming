27d96953e39b4bc6d118c01c76ee5a79ef33714c
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-06-13 20:06:17 +0000

    Task 4: Print square with TDD

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 4-print_square
+Contains a function that prints a square.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with the character #.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    
+    for i in range(size):
+        print("#" * size)
