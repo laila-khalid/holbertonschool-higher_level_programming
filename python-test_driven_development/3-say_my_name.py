d45bc38dceeecaa8edeba32255fb30e221ae02ba
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-06-13 20:02:51 +0000

    Fix trailing space and add missing argument tests

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
 Module 3-say_my_name
-Contains a function that prints a name.
 """
 
 
@@ -13,4 +12,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name).rstrip())
