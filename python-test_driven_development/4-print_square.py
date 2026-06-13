3777c3f0dd3d18a7823cb3b113126d35bbf4163f
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-06-13 20:07:50 +0000

    Fix Pycodestyle and add missing argument test

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-"""
-Module 4-print_square
-Contains a function that prints a square.
-"""
+"""Module 4-print_square"""
 
 
 def print_square(size):
-    """
-    Prints a square with the character #.
-    """
+    """Prints a square with the character #"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    
     for i in range(size):
         print("#" * size)
