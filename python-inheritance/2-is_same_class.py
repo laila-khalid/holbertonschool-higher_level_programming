8b6cc8136b972fa8005b29dddae959ec2e32ffb2
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-06-27 19:22:13 +0000

    Task 2: Exact same object

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that defines a class-checking function."""
+
+
+def is_same_class(obj, a_class):
+    """Check if an object is exactly an instance of a specified class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+
+    Returns:
+        bool: True if obj is exactly an instance of a_class, False otherwise.
+    """
+    return type(obj) is a_class
