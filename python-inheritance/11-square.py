4ae313bff1e54002aa87d8e35c2a63d8eb824d12
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-06-27 20:26:44 +0000

    Task 11: Square #2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module for Square class."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A subclass representing a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return the printable string representation of the square."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
