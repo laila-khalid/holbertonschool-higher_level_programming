fb9593bfedafb4bdd6e8e21386e008495a65551c
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-07-04 19:10:02 +0000

    Fix pycodestyle line length for Task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-Module that returns the dictionary description with simple data structure
-(list, dictionary, string, integer and boolean) for JSON serialization of an object.
+Module that returns the dictionary description with simple data
+structure (list, dictionary, string, integer and boolean)
+for JSON serialization of an object.
 """
 
 
