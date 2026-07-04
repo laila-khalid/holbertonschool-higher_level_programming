c1c34050b83a42d7f48817822245942560396290
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-07-04 18:20:23 +0000

    Task 2: Append to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module for appending a string to a text file.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string at the end of a text file (UTF8) and returns
+    the number of characters added.
+    """
+    with open(filename, mode='a', encoding='utf-8') as f:
+        return f.write(text)
