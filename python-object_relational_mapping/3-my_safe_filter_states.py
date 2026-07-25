ec39bbd11c629456a5946829ecff07d1fa9ad16d
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-07-25 20:21:16 +0000

    Fix checker 5: Case sensitivity and exact sorting

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,13 +3,15 @@
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
     
-    query = "SELECT * FROM states WHERE name = BINARY %s ORDER BY id ASC"
+    query = (
+        "SELECT * FROM states WHERE name = BINARY %s "
+        "ORDER BY states.id ASC"
+    )
     cursor.execute(query, (sys.argv[4],))
     
     for row in cursor.fetchall():
