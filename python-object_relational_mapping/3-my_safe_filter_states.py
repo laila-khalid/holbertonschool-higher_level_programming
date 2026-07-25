318fdfdff82c1598a1adcf1ae149c11b4450be92
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-07-25 20:41:33 +0000

    Fix checker static analysis for sys.argv

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,18 +4,14 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    
-    query = (
-        "SELECT * FROM states WHERE name = BINARY %s "
-        "ORDER BY states.id ASC"
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
+        (sys.argv[4],)
     )
-    cursor.execute(query, (sys.argv[4],))
-    
     for row in cursor.fetchall():
         print(row)
-        
     cursor.close()
     db.close()
