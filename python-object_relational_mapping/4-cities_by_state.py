60ff494b60b01169978fe8e31a60b70b0b7ed072
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-07-25 20:21:39 +0000

    Fix checker 5: Add utf8 charset to handle special characters

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""
-Lists all cities from the database hbtn_0e_4_usa
-"""
+"""Lists all cities from the database"""
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306,
+                         charset="utf8")
     cursor = db.cursor()
     
-    query = ("SELECT cities.id, cities.name, states.name "
-             "FROM cities JOIN states ON cities.state_id = states.id "
-             "ORDER BY cities.id ASC")
-    
+    query = (
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC"
+    )
     cursor.execute(query)
     
     for row in cursor.fetchall():
