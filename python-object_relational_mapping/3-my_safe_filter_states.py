1f4df28ff2b6b907dfa7244a50e275943360d41c
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-07-25 20:09:22 +0000

    Fix checker 4 try 2

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """
-Takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
+Script that takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
 Safe from MySQL injections!
 """
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306,
-                         charset="utf8")
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id ASC", (sys.argv[4],))
+    
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+    cursor.execute(query, (sys.argv[4],))
+    
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+        
     cursor.close()
     db.close()
