fe6f090512ca33e55c0466b5d00a4a367dc693d4
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-07-25 19:47:07 +0000

    2. Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -6,13 +6,15 @@ of hbtn_0e_0_usa where name matches the argument.
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cursor = db.cursor()
     
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"
+    query = (
+        "SELECT * FROM states WHERE name = BINARY '{}' "
+        "ORDER BY states.id ASC"
+    )
     cursor.execute(query.format(sys.argv[4]))
     
     rows = cursor.fetchall()
