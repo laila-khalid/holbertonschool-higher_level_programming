6f86dd7a9dff42bfaf64c63ddaedc9041d2a21d9
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-07-25 20:07:01 +0000

    Fix checker 4

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,15 +9,12 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306,
+                         charset="utf8")
     cursor = db.cursor()
-    
-    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
-    cursor.execute(query, (sys.argv[4],))
-    
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id ASC", (sys.argv[4],))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
-        
     cursor.close()
     db.close()
